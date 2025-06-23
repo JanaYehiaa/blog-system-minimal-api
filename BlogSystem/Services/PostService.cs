7906ecc4fc1b963f3b1121aaf04388682bf31407
janaYahyaa <jana7yehia@gmail.com>
2025-06-23 18:53:26 +0300

    refactor: remove Mapster and map PostCreateDTO manually

--- BlogSystem/Services/PostService.cs
+++ BlogSystem/Services/PostService.cs
@@ -1,26 +1,33 @@
-    public class PostService
+using Mapster;
+
+public class PostService
+{
+    public Post CreatePost(PostCreateDTO dto, string authorUsername)
     {
-        public Post CreatePost(PostCreateDTO dto, string authorUsername)
-        {
-            var post = dto.Adapt<Post>();
+        Post post = new();
 
-            post.Id = GeneratePostId();
-            post.Slug = Slugify(dto.Title);
-            post.AuthorUsername = authorUsername;
-            post.CreatedAt = DateTime.UtcNow;
-            post.ModifiedAt = null;
-            post.Status = PostStatus.Draft;
-
-            return post;
-        }
-        private string Slugify(string title)
-        {
-            return title.ToLower().Replace(" ", "-");
-        }
-
-        private int GeneratePostId()
-        {
-            return PostStore.Posts.OrderByDescending(post => post.Id).FirstOrDefault().Id + 1;
-        }
+        post.Id = GeneratePostId();
+        post.Title = dto.Title;
+        post.Slug = Slugify(dto.Title);
+        post.Description = dto.Description;
+        post.Body = dto.Body;
+        post.CreatedAt = DateTime.UtcNow;
+        post.PublishedAt = null;
+        post.ModifiedAt = null;
+        post.AuthorUsername = authorUsername;
+        post.Status = PostStatus.Draft;
+        post.Metadata = dto.Metadata;
+        post.Assets = dto.Assets;
+        return post;
     }
 
+    private string Slugify(string title)
+    {
+        return title.ToLower().Replace(" ", "-");
+    }
+
+    private int GeneratePostId()
+    {
+        return PostStore.Posts.OrderByDescending(post => post.Id).FirstOrDefault().Id + 1;
+    }
+}
