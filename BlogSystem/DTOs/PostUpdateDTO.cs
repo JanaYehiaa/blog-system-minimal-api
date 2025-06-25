f0b364d30d20790448cb294bd47870b4e12f39f1
janaYahyaa <jana7yehia@gmail.com>
2025-06-25 17:51:41 +0300

    feat: make PostUpdatedDto fields nullable for selective updates

--- BlogSystem/DTOs/PostUpdateDTO.cs
+++ BlogSystem/DTOs/PostUpdateDTO.cs
@@ -1,10 +1,9 @@
 public class PostUpdateDTO
 {
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public string Body { get; set; } = string.Empty;
-    public PostStatus Status { get; set; }
-    public MetaData Metadata { get; set; } = new();
-    public List<string> Assets { get; set; } = new();
-
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? Body { get; set; }
+    public PostStatus? Status { get; set; }
+    public MetaData? Metadata { get; set; }
+    public List<string>? Assets { get; set; }
 }
